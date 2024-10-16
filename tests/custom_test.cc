8aefdfcf3a46a541e7650735ffc132e7d2a29992
jeff <jeffyan167@gmail.com>
2024-10-16 23:06:24 +0800

    添加了自己的测试

---
+++ tests/custom_test.cc
@@ -0,0 +1,21 @@
+#include "../src/byte_stream.hh"
+#include <string_view>
+#include <iostream>
+using namespace std;
+
+int main() {
+  ByteStream bs(2);
+  Writer w = bs.writer();
+  Reader r = bs.reader();
+
+  w.push("");
+  w.push("cat");
+  w.push("");
+  cout << r.peek()<< endl;
+  cout << r.bytes_buffered()<< endl;
+  cout << r.peek()<< endl;
+  r.pop(1);
+  w.push("");
+  cout << r.peek()<< endl;
+  cout << r.bytes_buffered()<< endl;
+}
