e80d5aeb6960ca24d2d233f204d66b711766ee38
jeff <jeffyan167@gmail.com>
2024-10-29 17:29:01 +0800

    完成了unwrap将uint32解包为uint64

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -13,7 +13,13 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  Wrap32 target = wrap(checkpoint, zero_point);
+  int32_t offset = raw_value_ - target.raw_value_;
+  int64_t ans = checkpoint + offset;
+  if (ans >= 0) {
+    return ans;
+  }
+  else {
+    return ans + (1ul << 32);
+  }
 }
